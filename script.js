5eac780ca74cda93ad2b064e5bb2d8c64246781f
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-08-17 21:24:59 +0530

    objects

---
+++ script.js
@@ -0,0 +1,16 @@
+// Objects
+const jonasArray = [
+  "Jonas", //bracket type objects
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann", // dot type objects
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
